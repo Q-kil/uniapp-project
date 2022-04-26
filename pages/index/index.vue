8e1200176930f7bcae86ec3f4c95c2f86943faa6
Q-kil <kaifawebb@gmail.com>
2022-04-26 17:08:14 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="content">
+		<uni-list>
+			<uni-list-item v-for="(item,index) in news" :title="item.title" :note="item.created_at" name="" :thumb="item.author_avatar"
+			:clickable="true" @click="openInfo(item.post_id)">
+			</uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				news: []
+			}
+		},
+		onLoad() {
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news',
+				method: 'GET',
+				data: {},
+				success: res => {
+					console.log('res', res);
+					this.news = res.data;
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+			// localStorage.setItem('test', JSON.stringify({a:1}));
+			uni.setStorage({
+				key: 'key',
+				data: '{a: 1}'
+			})
+		},
+		methods: {
+			openInfo(id){
+				console.log('open', id);
+				uni.navigateTo({
+					url: '../info/info?id=' + id
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/info/info.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		111
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(e) {
+			console.log('e', e);
+			const id = e.id;
+			uni.request({
+				url: 'https://unidemo.dcloud.net.cn/api/news/36kr' + id,
+				method: 'GET',
+				data: {},
+				success: res => {
+					console.log('res info', res);
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.content{
+	padding: 10rpx 2%;
+	width: 96%;
+	flex-wrap: wrap;
+}
+.title{
+	line-height: 2em;
+	font-weight: 700;
+	font-size: 38rpx;
+}
+</style>
